46985fdde7d2990a1a9c84bc772e201dceed634d
Arash <arash382.jb@gmail.com>
2023-09-17 01:20:26 +0400

    add InfoBox

--- src/app/[locale]/_components/Layout/index.tsx
+++ src/app/[locale]/_components/Layout/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import InfoBox from "../InfoBox";
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <InfoBox />
     </>
   );
 };
